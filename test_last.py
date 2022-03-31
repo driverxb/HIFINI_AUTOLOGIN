53c45b098f2cc5e9246da8c03960cb3d3968ab65
driverxb <54368979+driverxb@users.noreply.github.com>
2022-03-31 17:28:26 +0800

    Update test_last.py

--- test_last.py
+++ test_last.py
@@ -1,11 +1,23 @@
 from datetime import datetime
 import time
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-if __name__=='__main__': 
-    driver = webdriver.Chrome()
-    driver.get("https://hifini.com") 
+from selenium.webdriver.chrome.options import Options # 这个是用来实现无可视化和反检测
+
+if __name__=='__main__':
+    # 设置Chromedriver参数,实现让selenium实现无界面和不被检测到的风险
+    chrome_options = Options()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging','enable-automation'])
+    chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
+    chrome_options.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
+    chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+    #chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+    chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+    chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面.
+    chrome_options.add_argument('--incognito')  # 隐身模式（无痕模式）
+    chrome_options.add_argument('--disable-infobars')  # 禁用浏览器正在被自动化程序控制的提示
+    driver = webdriver.Chrome(options=chrome_options)
+    #driver = webdriver.Chrome()
     listcookie = [
         {
             'name': 'Hm_lvt_4ab5ca5f7f036f4a4747f1836fffe6f2',
@@ -24,7 +36,7 @@ if __name__=='__main__':
     sg_sign = driver.find_element(By.ID,"sg_sign") # 找到签到签到标签  
     while True:
         time_now = time.strftime("%H:%M:%S",time.localtime())
-        if time_now == "09:24:00" :
+        if time_now == "09:30:00" :
             sg_sign.click() # 对签到标签进行点击操作
             break # 退出循环 
     sleep(1)
