0a750f55d4c1734d034a3df1f138ced11d2ae8e2
driverxb <54368979+driverxb@users.noreply.github.com>
2022-03-31 16:28:01 +0800

    Update test_chromedriver.py

--- test_chromedriver.py
+++ test_chromedriver.py
@@ -1,15 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options # 这个是用来实现无可视化和反检测
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "d:/a/hifini_autologin/hifini_autologin/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
-driver.get("https://www.baidu.com")
-print(driver.title.encode("utf-8"))
-driver.quit()
+if __name__=='__main__':
+    # 设置Chromedriver参数,实现让selenium实现无界面和不被检测到的风险
+    chrome_options = Options()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging','enable-automation'])
+    chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
+    chrome_options.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
+    chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+    #chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+    chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+    chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面.
+    chrome_options.add_argument('--incognito')  # 隐身模式（无痕模式）
+    chrome_options.add_argument('--disable-infobars')  # 禁用浏览器正在被自动化程序控制的提示
+    chromedriver = "d:/a/hifini_autologin/hifini_autologin/chromedriver"
+    os.environ["webdriver.chrome.driver"] = chromedriver
+    driver = webdriver.Chrome(options=chrome_options,executable_path=chromedriver)
+    driver.get("https://www.baidu.com")
+    print(driver.title.encode("utf-8"))
+    driver.quit()
